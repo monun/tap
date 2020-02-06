4db7064c7e6d100ea874bbabb9bad9ada36ef7ed
noonmaru <noonmaru10@gmail.com>
2020-02-07 07:41:22 +0900

    support 1.14.4

---
+++ v1_14_R1/src/main/kotlin/com/github/noonmaru/tap/v1_14_R1/fake/NMSFakeSupport.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2020 Noonmaru
+ *
+ * Licensed under the General Public License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/gpl-2.0.php
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.github.noonmaru.tap.v1_14_R1.fake
+
+import com.github.noonmaru.tap.fake.FakeSupport
+import net.minecraft.server.v1_14_R1.IRegistry
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.craftbukkit.v1_14_R1.CraftServer
+import org.bukkit.craftbukkit.v1_14_R1.CraftWorld
+import org.bukkit.craftbukkit.v1_14_R1.entity.CraftEntity
+import org.bukkit.entity.Entity
+
+/**
+ * @author Nemo
+ */
+class NMSFakeSupport : FakeSupport {
+
+    override fun getNetworkId(entity: Entity): Int {
+        entity as CraftEntity
+
+        return IRegistry.ENTITY_TYPE.a(entity.handle.entityType)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : Entity> createEntity(entityClass: Class<out Entity>): T? {
+        return NMSEntityTypes.findType(entityClass)?.run {
+            val world = (Bukkit.getServer() as CraftServer).server.worlds.first()
+            this.a(world)?.bukkitEntity as T
+        }
+    }
+
+    override fun isInvisible(entity: Entity): Boolean {
+        entity as CraftEntity
+        val nmsEntity = entity.handle
+
+        return nmsEntity.isInvisible
+    }
+
+    override fun setInvisible(entity: Entity, invisible: Boolean) {
+        entity as CraftEntity
+        val nmsEntity = entity.handle
+
+        nmsEntity.isInvisible = invisible
+    }
+
+    override fun setPosition(entity: Entity, loc: Location) {
+        entity as CraftEntity
+        val nmsEntity = entity.handle
+
+        loc.run {
+            nmsEntity.world = (world as CraftWorld).handle
+            nmsEntity.setPosition(x, y, z)
+        }
+    }
+
+    override fun setPositionAndRotation(
+        entity: Entity,
+        loc: Location
+    ) {
+        entity as CraftEntity
+        val nmsEntity = entity.handle
+
+        loc.run {
+            nmsEntity.world = (world as CraftWorld).handle
+            nmsEntity.setPositionRotation(x, y, z, yaw, pitch)
+        }
+    }
+}
