8c742f7e41e7defab4c908d9cfa6f0d79b612bf4
Noonmaru <noonmaru10@gmail.com>
2020-05-25 08:50:09 +0900

    kotlin 1.3.61 -> 1.3.72

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,24 @@
 /*
- * Copyright (c) 2020 Noonmaru
+ * Copyright (c) $date.year Noonmaru
  *
- * Licensed under the General Public License, Version 2.0 (the "License");
+ *  Licensed under the General Public License, Version 3.0 (the "License");
  *  you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * https://opensource.org/licenses/gpl-2.0.php
+ * https://opensource.org/licenses/gpl-3.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import org.gradle.internal.jvm.Jvm
 
 plugins {
     id("com.github.johnrengelman.shadow") version "5.2.0"
-    kotlin("jvm") version "1.3.61"
+    kotlin("jvm") version "1.3.72"
     `maven-publish`
 }
 
@@ -132,8 +132,8 @@ tasks {
     shadowJar {
         archiveClassifier.set("dist")
     }
-//    create<Copy>("distJar") {
-//        from(shadowJar)
-//        into("W:\\Servers\\test\\plugins")
-//    }
+    create<Copy>("distJar") {
+        from(shadowJar)
+        into("W:\\Servers\\test\\plugins")
+    }
 }
