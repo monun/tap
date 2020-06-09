d4035cadd6b4290b1a4e37b0cba5049d8224307b
noonmaru <noonmaru10@gmail.com>
2020-06-10 04:41:23 +0900

    preinit parallel

--- api/src/main/kotlin/com/github/noonmaru/tap/fake/FakeManager.kt
+++ api/src/main/kotlin/com/github/noonmaru/tap/fake/FakeManager.kt
@@ -1,19 +1,17 @@
 /*
+ * Copyright (c) $date.year Noonmaru
  *
- *  * Copyright (c) 2020 Noonmaru
- *  *
- *  * Licensed under the General Public License, Version 3.0 (the "License");
- *  *  you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  * https://opensource.org/licenses/gpl-3.0
- *  *
- *  *  Unless required by applicable law or agreed to in writing, software
- *  *  distributed under the License is distributed on an "AS IS" BASIS,
- *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  *  See the License for the specific language governing permissions and
- *  *  limitations under the License.
+ *  Licensed under the General Public License, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ * https://opensource.org/licenses/gpl-3.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.github.noonmaru.tap.fake
@@ -23,7 +21,7 @@ import org.bukkit.block.data.BlockData
 import org.bukkit.entity.*
 import java.util.*
 
-class FakeManager : Runnable {
+class FakeManager {
 
     private val entities = HashSet<FakeEntity>()
 
@@ -115,7 +113,7 @@ class FakeManager : Runnable {
     /**
      * 이 메서드를 Tick마다 호출해주세요!
      */
-    override fun run() {
+    fun update() {
         updateEntities()
         updateTrackers()
     }
