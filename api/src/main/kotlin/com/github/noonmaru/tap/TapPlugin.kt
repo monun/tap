0911b72f55caa215700aa9fa88b4ba4d68deb6c7
noonmaru <noonmaru10@gmail.com>
2020-06-13 10:23:01 +0900

    fake add passenger feature

--- api/src/main/kotlin/com/github/noonmaru/tap/TapPlugin.kt
+++ api/src/main/kotlin/com/github/noonmaru/tap/TapPlugin.kt
@@ -17,18 +17,6 @@
 package com.github.noonmaru.tap
 
 import com.github.noonmaru.tap.attach.Tools
-import com.github.noonmaru.tap.fake.FakeEntity
-import com.github.noonmaru.tap.fake.FakeServer
-import com.github.noonmaru.tap.fake.invisible
-import org.bukkit.Bukkit
-import org.bukkit.Material
-import org.bukkit.entity.ArmorStand
-import org.bukkit.event.EventHandler
-import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerInteractEvent
-import org.bukkit.event.player.PlayerJoinEvent
-import org.bukkit.inventory.EquipmentSlot
-import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
@@ -37,56 +25,5 @@ import org.bukkit.plugin.java.JavaPlugin
 class TapPlugin : JavaPlugin() {
     override fun onEnable() {
         Tools.loadAttachLibrary(dataFolder)
-
-        debug()
-    }
-
-    private val fakeServer = FakeServer.create()
-    private var fakeEntity: FakeEntity? = null
-
-    private fun debug() {
-        Bukkit.getOnlinePlayers().forEach { fakeServer.addPlayer(it) }
-        server.apply {
-            pluginManager.registerEvents(EventListener(), this@TapPlugin)
-            scheduler.runTaskTimer(this@TapPlugin, Runnable {
-                fakeEntity?.let { entity ->
-                    Bukkit.getPlayerExact("Heptagram")?.let { player ->
-                        entity.moveTo(player.location.apply {
-                            add(direction.multiply(4.5))
-                        })
-                    }
-                }
-
-                fakeServer.update()
-            }, 0L, 1L)
-        }
-    }
-
-    inner class EventListener : Listener {
-        @EventHandler
-        fun onPlayerInteract(event: PlayerInteractEvent) {
-            fakeEntity?.remove()
-            fakeEntity = fakeServer.spawnEntity(event.player.location, ArmorStand::class.java).apply {
-                metadata<ArmorStand> {
-                    invisible = true
-                    isMarker = true
-//                    isGlowing = true
-                    true
-                }
-                armorStandItem {
-                    setItem(EquipmentSlot.HEAD, ItemStack(Material.STONE))
-                    true
-                }
-            }
-        }
-
-        @EventHandler
-        fun onPlayerJoin(event: PlayerJoinEvent) {
-            fakeServer.addPlayer(event.player)
-        }
-    }
-
-    override fun onDisable() {
-        fakeServer.clear()
     }
 }
