e3377a673d5a6b8c5cbe7742cd47d782a5f01a78
PatrickKR <jehyeokchoe@gmail.com>
2020-06-26 20:27:46 +0900

    Fix Invalid API Naming  - folder v1_13_R1 renamed to v1_13_R2 (1.13.1, 1.13.2)  - new support for v1_13_R1 (1.13)  - Version 2.4.1 -> 2.5  - Removed unnecessary dependency/repository, added mavenLocal to allprojects

--- v1_15_R1/build.gradle.kts
+++ v1_15_R1/build.gradle.kts
@@ -1,8 +1,3 @@
-repositories {
-    mavenLocal() // nms spigot
-}
-
 dependencies {
-    compileOnly("com.destroystokyo.paper:paper-api:1.15.2-R0.1-SNAPSHOT")
     compileOnly("org.spigotmc:spigot:1.15.2-R0.1-SNAPSHOT")
 }
