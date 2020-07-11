07527e6cddf79eead4bab0d650a476634e7d0eb9
noonmaru <noonmaru10@gmail.com>
2020-07-12 08:12:28 +0900

    Use Packet instead of EffectPacket

--- api/src/main/kotlin/com/github/noonmaru/tap/effect/FireworkSupport.kt
+++ api/src/main/kotlin/com/github/noonmaru/tap/effect/FireworkSupport.kt
@@ -1,24 +1,22 @@
 /*
+ * Copyright (c) $date.year Noonmaru
  *
- *  * Copyright (c) 2020 Noonmaru
- *  *
- *  * Licensed under the General Public License, Version 3.0 (the "License");
- *  *  you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  * https://opensource.org/licenses/gpl-3.0
- *  *
- *  *  Unless required by applicable law or agreed to in writing, software
- *  *  distributed under the License is distributed on an "AS IS" BASIS,
- *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  *  See the License for the specific language governing permissions and
- *  *  limitations under the License.
+ *  Licensed under the General Public License, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ * https://opensource.org/licenses/gpl-3.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.github.noonmaru.tap.effect
 
-import com.github.noonmaru.tap.protocol.EffectPacket
+import com.github.noonmaru.tap.protocol.Packet
 import com.github.noonmaru.tap.protocol.sendServerPacket
 import org.bukkit.FireworkEffect
 import org.bukkit.Location
@@ -27,13 +25,13 @@ import org.bukkit.entity.Player
 import org.bukkit.util.BoundingBox
 
 fun Player.playFirework(loc: Location, effect: FireworkEffect) {
-    EffectPacket.firework(loc, effect).forEach { packet ->
+    Packet.spawnFireworkParticles(loc, effect).forEach { packet ->
         sendServerPacket(packet)
     }
 }
 
 fun World.playFirework(loc: Location, effect: FireworkEffect, distance: Double = 128.0) {
-    val packets = EffectPacket.firework(loc, effect)
+    val packets = Packet.spawnFireworkParticles(loc, effect)
 
     val world = loc.world
     val box = BoundingBox.of(loc, distance, distance, distance)
