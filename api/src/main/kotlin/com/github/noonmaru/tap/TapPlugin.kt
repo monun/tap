25c9aae46f3643ab0bc6506a7a03cbbdcf0b5b20
noonmaru <noonmaru10@gmail.com>
2020-07-18 16:36:32 +0900

    Add debug code (Annotated)

--- api/src/main/kotlin/com/github/noonmaru/tap/TapPlugin.kt
+++ api/src/main/kotlin/com/github/noonmaru/tap/TapPlugin.kt
@@ -25,5 +25,93 @@ import org.bukkit.plugin.java.JavaPlugin
 class TapPlugin : JavaPlugin() {
     override fun onEnable() {
         Tools.loadAttachLibrary(dataFolder)
+
+//        debug()
     }
+
+//    private fun debug() {
+//        Debug(this).also { debug ->
+//            server.apply {
+//                pluginManager.registerEvents(debug, this@TapPlugin)
+//                scheduler.runTaskTimer(this@TapPlugin, debug, 0L, 1L)
+//            }
+//        }
+//    }
 }
+
+//class Debug(plugin: JavaPlugin) : Listener, Runnable {
+//    private val fakeServer = FakeServer.create(plugin)
+//
+//    override fun run() {
+//        fakeServer.update()
+//    }
+//
+//    @EventHandler
+//    fun onPlayerJoin(event: PlayerJoinEvent) {
+//        fakeServer.addPlayer(event.player)
+//    }
+//
+//    @EventHandler
+//    fun onPlayerInteract(event: PlayerInteractEvent) {
+//        val location = event.player.eyeLocation
+//        val offset = Vector(-32.0, 0.0, 20.0).rotateAroundY(-Math.toRadians(location.yaw.toDouble()))
+//        location.add(offset)
+//        val v = Vector(5.0, 0.0, 0.0).rotateAroundY(-Math.toRadians(location.yaw.toDouble()))
+//
+//        val projectile = TestProjectile().apply {
+//            mount(fakeServer.spawnEntity(location, ArmorStand::class.java), Vector(0.0, -1.62, 0.0))
+//            velocity = v
+//        }
+//
+//        fakeServer.launch(location, projectile)
+//    }
+//}
+//
+//class TestProjectile : FakeProjectile(100, 100.0) {
+//    override fun onMove(movement: Movement) {
+//        val to = movement.to
+//
+//        to.world.spawnParticle(
+//            Particle.FLAME,
+//            to,
+//            0,
+//            0.0,
+//            0.0,
+//            0.0,
+//            0.0,
+//            null,
+//            true
+//        )
+//    }
+//
+//    override fun onTrail(from: Location, to: Location) {
+//        to.world.spawnParticle(
+//            Particle.VILLAGER_HAPPY,
+//            to.add(0.0, 0.25, 0.0),
+//            0,
+//            0.0,
+//            0.0,
+//            0.0,
+//            0.0,
+//            null,
+//            true
+//        )
+//
+//        if (from.world === to.world) {
+//            val v = from.vector(to)
+//            val length = v.normalizeAndLength()
+//
+//            from.world.rayTrace(from, v, length, FluidCollisionMode.NEVER, true,  0.5) { entity ->
+//                entity is LivingEntity
+//            }?.let { result ->
+//                val hit = result.hitPosition
+//                from.world.createExplosion(hit.x, hit.y, hit.z, 4.0F)
+//                remove()
+//            }
+//        }
+//    }
+//
+//    override fun onRemove() {
+//        passenger?.fakeEntity?.remove()
+//    }
+//}
