1a6a5f5f193b61132337ab1e642e5bded15948da
noonmaru <noonmaru10@gmail.com>
2020-07-27 21:18:44 +0900

    Implement ItemSupport

--- v1_16_R1/src/main/kotlin/com/github/noonmaru/tap/v1_16_R1/item/NMSItemSupport.kt
+++ v1_16_R1/src/main/kotlin/com/github/noonmaru/tap/v1_16_R1/item/NMSItemSupport.kt
@@ -17,13 +17,50 @@
 package com.github.noonmaru.tap.v1_16_R1.item
 
 import com.github.noonmaru.tap.item.ItemSupport
+import net.minecraft.server.v1_16_R1.DamageSource
+import net.minecraft.server.v1_16_R1.ItemStack
 import net.minecraft.server.v1_16_R1.NBTTagCompound
+import net.minecraft.server.v1_16_R1.PlayerInventory
+import org.bukkit.craftbukkit.v1_16_R1.CraftEquipmentSlot
+import org.bukkit.craftbukkit.v1_16_R1.entity.CraftPlayer
+import org.bukkit.craftbukkit.v1_16_R1.inventory.CraftInventoryPlayer
 import org.bukkit.craftbukkit.v1_16_R1.inventory.CraftItemStack
-import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.inventory.ItemStack as BukkitItemStack
+import org.bukkit.inventory.PlayerInventory as BukkitPlayerInventory
 
-class NMSItemSupport : ItemSupport() {
-    override fun saveToJsonString(item: ItemStack): String {
+class NMSItemSupport : ItemSupport {
+    override fun saveToJsonString(item: BukkitItemStack): String {
         val nmsItem = CraftItemStack.asNMSCopy(item)
         return nmsItem.save(NBTTagCompound()).toString()
+    }
+
+    override fun damageArmor(playerInventory: BukkitPlayerInventory, attackDamage: Double) {
+        val nmsInventory = (playerInventory as CraftInventoryPlayer).inventory
+
+        nmsInventory.a(DamageSource.LAVA, attackDamage.toFloat())
+    }
+
+    override fun damageSlot(playerInventory: BukkitPlayerInventory, slot: EquipmentSlot, damage: Int) {
+        val nmsInventory = (playerInventory as CraftInventoryPlayer).inventory
+        val nmsSlot = CraftEquipmentSlot.getNMS(slot)
+        val nmsItem = nmsInventory.getItem(slot)
+
+        if (!nmsItem.isEmpty) {
+            nmsItem.damage(damage, (playerInventory.holder as CraftPlayer).handle) { player ->
+                player.broadcastItemBreak(nmsSlot)
+            }
+        }
+    }
+}
+
+internal fun PlayerInventory.getItem(slot: EquipmentSlot): ItemStack {
+    return when (slot) {
+        EquipmentSlot.HAND -> itemInHand
+        EquipmentSlot.OFF_HAND -> extraSlots[0]
+        EquipmentSlot.FEET -> armorContents[0]
+        EquipmentSlot.LEGS -> armorContents[1]
+        EquipmentSlot.CHEST -> armorContents[2]
+        EquipmentSlot.HEAD -> armorContents[3]
     }
 }
