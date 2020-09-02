d809ff6a8075ef9978056588c6fa5009957703a9
Noonmaru <noonmaru10@gmail.com>
2020-09-02 23:14:03 +0900

    Add v1_16_2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020 Noonmaru
+ *
+ *  Licensed under the General Public License, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/gpl-3.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 rootProject.name = "tap"
 
 include(
@@ -5,5 +21,6 @@ include(
     "v1_13_R2",
     "v1_14_R1",
     "v1_15_R1",
-    "v1_16_R1"
+    "v1_16_R1",
+    "v1_16_R2"
 )
