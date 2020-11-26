026a13bfd19f2951f204967a1eda4ceb35ccc62a
Patrick Choe <mailpatrickkr@gmail.com>
2020-11-26 17:14:23 +0900

    자바 -> 코틀린 변환  - EventEntityProvider.java 는 전반적인 구조 리팩토링 없이는 변환이 힘들 것 같습니다.   해당 클래스에서 사용하는 EntityProvider 가 파라미터 없이 쓰이고 있기 때문에 (raw use)   이를 해결하기 위해 event 패키지를 전체적으로 바꿔야 합니다.

---
+++ api/src/main/kotlin/com/github/noonmaru/tap/event/EventTools.kt
@@ -0,0 +1,97 @@
+package com.github.noonmaru.tap.event
+
+import org.bukkit.event.Event
+import org.bukkit.plugin.IllegalPluginAccessException
+import java.lang.reflect.ParameterizedType
+import java.util.ArrayList
+import java.util.WeakHashMap
+
+object EventTools {
+    private val CUSTOM_PROVIDERS: MutableMap<Class<*>, EventEntityProvider> = WeakHashMap()
+    private val DEFAULT_PROVIDERS: Array<EventEntityProvider>
+
+    /**
+     * [Event]를 상속한 클래스들 중 [org.bukkit.event.HandlerList]가 있는 클래스를 찾아서 반환합니다.
+     */
+    @JvmStatic
+    fun getRegistrationClass(clazz: Class<*>): Class<*> {
+        return try {
+            clazz.getDeclaredMethod("getHandlerList")
+            clazz
+        } catch (e: NoSuchMethodException) {
+            if (clazz.superclass != null
+                    && clazz.superclass != Event::class.java
+                    && Event::class.java.isAssignableFrom(clazz.superclass)) {
+                getRegistrationClass(clazz.superclass.asSubclass(Event::class.java))
+            } else {
+                throw IllegalPluginAccessException("Unable to find handler list for event ${clazz.name}. Static getHandlerList method required!")
+            }
+        }
+    }
+
+    /**
+     * [DefaultProvider]에서 호환 가능한 제공자를 반환합니다.
+     *
+     * @param eventClass 찾아낼 클래스
+     * @return 호환되는 엔티티 제공자
+     * @see DefaultProvider
+     */
+    @JvmStatic
+    fun findDefaultProvider(eventClass: Class<*>): EventEntityProvider {
+        for (provider in DEFAULT_PROVIDERS) {
+            if (provider.eventClass.isAssignableFrom(eventClass))
+                return provider
+        }
+
+        throw IllegalArgumentException("Not found DefaultProvider for $eventClass")
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    @JvmStatic
+    fun getOrCreateCustomProvide(providerClass: Class<*>): EventEntityProvider {
+        return CUSTOM_PROVIDERS.computeIfAbsent(providerClass) { clazz: Class<*> ->
+            try {
+                return@computeIfAbsent EventEntityProvider(clazz.asSubclass(EntityProvider::class.java).newInstance())
+            } catch (e: InstantiationException) {
+                throw AssertionError(e)
+            } catch (e: IllegalAccessException) {
+                throw AssertionError(e)
+            }
+        }
+    }
+
+    @JvmStatic
+    fun getGenericEventType(providerClass: Class<*>): Class<*> {
+        var clazz = providerClass
+        val prefix = EntityProvider::class.java.name + "<" //제너릭 타임 이름은 ClassName<Type>으로 반환됨
+        val genericInterfaces = clazz.genericInterfaces
+
+        do {
+            for (genericInterface in genericInterfaces) {
+                if (genericInterface.typeName.startsWith(prefix)) {
+                    return (genericInterface as ParameterizedType).actualTypeArguments[0] as Class<*>
+                }
+            }
+        } while (clazz.superclass.also { clazz = it } != Any::class.java)
+
+        throw IllegalArgumentException("$clazz is not EntityProvider")
+    }
+
+    init {
+        // 기본 개체 제공자 초기화
+        val classes = DefaultProvider::class.java.declaredClasses
+        val defaultProviders: MutableList<EventEntityProvider> = ArrayList(classes.size)
+
+        for (clazz in classes) {
+            if (EntityProvider::class.java.isAssignableFrom(clazz)) {
+                try {
+                    defaultProviders.add(EventEntityProvider(clazz.newInstance() as EntityProvider<*>))
+                } catch (e: Exception) {
+                    throw AssertionError(e)
+                }
+            }
+        }
+
+        DEFAULT_PROVIDERS = defaultProviders.toTypedArray()
+    }
+}
