49f7aaaef267fb4aaf9be4d5465ea3f9ed4e17d7
Monun <monun1010@gmail.com>
2021-02-09 23:18:02 +0900

    New kotlin download url

--- paper.ps1
+++ paper.ps1
@@ -12,7 +12,7 @@ $Version = "1.16.5"
 
 # Plugins
 $Plugins = @(
-"https://github.com/monun/kotlin-plugin/releases/latest/download/Kotlin.jar",
+"https://github.com/monun/kotlin-plugin/releases/latest/download/Kotlin-1.4.30.jar",
 "https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/target/ProtocolLib.jar"
 )
 
