7c106b85ecdfccdfa130b27e4cfe96e9823e6323
monun <monun1010@gmail.com>
2021-03-18 22:15:01 +0900

    :white_check_mark: TEST: Ticker 테스트코드 추가
    Plank: 단일 태스크 & 딜레이 태스크 & 반복 태스크 작동 테스트
    Precision: 시간 지연시 반복 호출 작동 테스트
    Flex: 시간 지연시 스킵 테스트

---
+++ api/src/test/kotlin/com/github/monun/tap/task/TickerTest.kt
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2021 Monun
+ *
+ * Licensed under the Apache License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-3.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.monun.tap.task
+
+import org.junit.Test
+import org.mockito.Mockito.*
+
+class TickerTest {
+    @Test
+    fun testPlank() {
+        val runTask = mock(Runnable::class.java)
+        val runTaskLater = mock(Runnable::class.java)
+        val runTaskTimer = mock(Runnable::class.java)
+        val tickerPlank = Ticker.plank()
+
+        tickerPlank.apply {
+            runTask(runTask)
+            runTask(runTaskLater, 1L)
+            runTaskTimer(runTaskTimer, 1L, 1L)
+        }
+
+        tickerPlank.run()
+        verify(runTask).run()
+        verifyNoInteractions(runTaskLater, runTaskTimer)
+
+        tickerPlank.run()
+        verifyNoMoreInteractions(runTask)
+        verify(runTaskLater).run()
+        verify(runTaskTimer).run()
+
+        tickerPlank.run()
+        verifyNoMoreInteractions(runTask)
+        verifyNoMoreInteractions(runTaskLater)
+        verify(runTaskTimer, times(2)).run()
+    }
+
+    @Test
+    fun testPrecision() {
+        val tickMillis = 10L
+        val runTaskLater = mock(Runnable::class.java)
+        val runTaskTimer = mock(Runnable::class.java)
+
+        val tickerPrecision = Ticker.precision(System::nanoTime to tickMillis * 1000L * 1000L)
+        tickerPrecision.apply {
+            runTask(runTaskLater, 5L)
+            runTaskTimer(runTaskTimer, 1L, 1L)
+        }
+        Thread.sleep(tickMillis * 10L)
+        tickerPrecision.run()
+
+        verify(runTaskLater).run()
+        verify(runTaskTimer, times(tickerPrecision.currentTicks.toInt())).run() // 틱마다 모두 호출
+    }
+
+    @Test
+    fun testFlex() {
+        val tickMillis = 10L
+        val runTaskLater = mock(Runnable::class.java)
+        val runTaskTimer = mock(Runnable::class.java)
+
+        val tickerFlex = Ticker.flex(System::nanoTime to tickMillis * 1000L * 1000L)
+        tickerFlex.apply {
+            runTask(runTaskLater, 5L)
+            runTaskTimer(runTaskTimer, 1L, 1L)
+        }
+        Thread.sleep(tickMillis * 10L)
+        tickerFlex.run()
+
+        verify(runTaskLater).run()
+        verify(runTaskTimer).run()
+    }
+}
