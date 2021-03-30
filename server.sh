5922bb6aad61bd6ac35d6e41c9298d0ef05cb613
monun <monun1010@gmail.com>
2021-03-31 01:05:14 +0900

    :hammer: 서버 실행 스크립트에서 Kotlin 1.4.32 플러그인을 다운로드

--- server.sh
+++ server.sh
@@ -5,7 +5,7 @@
 server=paper
 version=1.16.5
 plugins=(
-    'https://github.com/monun/kotlin-plugin/releases/latest/download/Kotlin-1.4.31.jar'
+    'https://github.com/monun/kotlin-plugin/releases/latest/download/Kotlin-1.4.32.jar'
     'https://github.com/monun/auto-update/releases/latest/download/AutoUpdate.jar'
     'https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/target/ProtocolLib.jar'
 )
