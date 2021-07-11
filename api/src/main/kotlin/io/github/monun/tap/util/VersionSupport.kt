a094a14ba41cd46669dec3df0cc1700de8ded27c
dolphin2410 <80996692+dolphin2410@users.noreply.github.com>
2021-07-11 16:55:50 +0900

    Added gradle-version-identifier comparison support
    https://docs.gradle.org/current/userguide/dependency_management.html#declaring_versions를 바탕으로 identifier 비교를 추가하였습니다.

--- api/src/main/kotlin/io/github/monun/tap/util/VersionSupport.kt
+++ api/src/main/kotlin/io/github/monun/tap/util/VersionSupport.kt
@@ -24,18 +24,68 @@ import kotlin.math.max
  * 0.1 < 0.1.1
  *
  * 0.1 == 0.01
+ * 
+ * 0.1-beta < 0.1-release
+ *
+ * 0.1-final < 0.1
+ * 
  */
+
+enum class MavenVersionIdentifier(val priority: Int) {
+    NONE(10),
+    RELEASE(9),
+    GA(8),
+    FINAL(7),
+    SNAPSHOT(6),
+    RC(5),
+    ZETA(4),
+    BETA(3),
+    ALPHA(2),
+    DEV(1)
+}
+
+private val String.identifier: MavenVersionIdentifier
+    get() {
+        if(equals("0")) {
+            return MavenVersionIdentifier.NONE
+        } else {
+            return try {
+                MavenVersionIdentifier.valueOf(uppercase())
+            } catch (e: IllegalArgumentException) {
+                throw RuntimeException("No such version identifier found: ${this}")
+            }
+        }
+    }
+
+private val String.isValidLong: Boolean 
+    get() {
+        try {
+            toLong(0x10)
+            return true
+        }
+        catch (e: NumberFormatException) {
+            return false
+        }
+    }
 infix fun String.compareVersion(other: String): Int {
-    val split = split('.')
-    val otherSplit = other.split('.')
+    if(!matches("""^\d+(\.\d+)*(\-[a-zA-Z]*)?$""".toRegex()) || !other.matches("""^\d+(\.\d+)*(\-[a-zA-Z]*)?$""".toRegex())) {
+       throw RuntimeException("버전 양식에 맞지 않습니다.");
+    }
+    val split = replace("-", ".").split('.')
+    val otherSplit = other.replace("-", ".").split('.')
 
     loop@ for (i in 0 until max(split.count(), otherSplit.count())) {
         val a = split.getOrNull(i) ?: "0"
         val b = otherSplit.getOrNull(i) ?: "0"
         var compare = 0
-
+        val isLastInIndex = i == max(split.count(), otherSplit.count()) - 1
+        
         kotlin.runCatching {
-            compare = a.toLong(0x10).compareTo(b.toLong(0x10))
+            compare = if(isLastInIndex && (!a.isValidLong || !b.isValidLong)) {
+                a.identifier.priority.compareTo(b.identifier.priority)
+            } else {
+                a.toLong(0x10).compareTo(b.toLong(0x10))
+            }
         }.onFailure {
             compare = AlphanumComparator.compare(a, b)
         }
