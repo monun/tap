0c7430c020578e1676c9df8b41c1b33ab18359a1
Monun <monun1010@gmail.com>
2021-09-06 04:40:01 +0900

    :hammer: 최신 서버 스크립트

--- debug-paper.sh
+++ debug-paper.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
 
-server='https://papermc.io/api/v1/paper/1.17.1/latest/download'
 plugins=(
-'https://github.com/monun/auto-reloader/releases/latest/download/AutoReloader.jar'
+  'https://github.com/monun/auto-reloader/releases/latest/download/AutoReloader.jar'
 )
 
 script=$(basename "$0")
 server_folder=".${script%.*}"
 mkdir -p "$server_folder"
 
-server_script="start.sh"
-server_config="$server_script.conf"
+start_script="start.sh"
+start_config="$start_script.conf"
 
-if [ ! -f "$server_folder/$server_script" ]; then
-  if [ -f ".server/$server_script" ]; then
-    cp ".server/$server_script" "$server_folder/$server_script"
+if [ ! -f "$server_folder/$start_script" ]; then
+  if [ -f ".server/$start_script" ]; then
+    cp ".server/$start_script" "$server_folder/$start_script"
   else
-    wget -qc -P "$server_folder" -N "https://raw.githubusercontent.com/monun/server-script/master/.server/$server_script"
+    wget -qc -P "$server_folder" -N "https://raw.githubusercontent.com/monun/server-script/paper/.server/$start_script"
   fi
 fi
 
 cd "$server_folder" || exit
 
-if [ ! -f "$server_config" ]; then
-    cat << EOF > $server_config
-server=$server
+if [ ! -f "$start_config" ]; then
+  cat <<EOF >$start_config
+version=1.17.1
+build=latest
 debug=true
-debug_port=25564
+debug_port=5005
 backup=false
 force_restart=false
 memory=16
 plugins=(
 EOF
-    for plugin in "${plugins[@]}"
-    do
-        echo "  \"$plugin\""   >> $server_config
-    done
-    echo ")" >> $server_config
+  for plugin in "${plugins[@]}"; do
+    echo "  \"$plugin\"" >>$start_config
+  done
+  echo ")" >>$start_config
 fi
 
-chmod +x ./$server_script
-./$server_script launch
+chmod +x ./$start_script
+./$start_script launch
