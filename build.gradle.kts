383a20858ea102605cf9ad276c71da2a24dc3910
Monun <monun1010@gmail.com>
2022-08-04 01:47:04 +0900

    :fire: Remove setupModules

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,29 +53,6 @@ listOf("api", "core").forEach { projectName ->
     }
 }
 
-tasks {
-    register<DefaultTask>("setupModules") {
-        doLast {
-            val defaultPrefix = "sample"
-            val projectPrefix = rootProject.name
-
-            if (defaultPrefix != projectPrefix) {
-                fun rename(suffix: String) {
-                    val from = "$defaultPrefix-$suffix"
-                    val to = "$projectPrefix-$suffix"
-                    file(from).takeIf { it.exists() }?.renameTo(file(to))
-                }
-
-                rename("api")
-                rename("core")
-                rename("dongle")
-                rename("plugin")
-                rename("publish")
-            }
-        }
-    }
-}
-
 idea {
     module {
         excludeDirs.add(file(".server"))
